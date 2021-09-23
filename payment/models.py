e0ae5dd3434cdde27c6c342ca3f4b973579977ec
5no0p <mmoaaa1989@hotmail.com>
2021-09-23 16:01:49 +0200

    migration & init api
    make migration and start ticket api

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.db.models.deletion import CASCADE
+from ticket.models import Ticket
+
+
+PAYE_STAUTS_CHOICES = (
+    ("F", "Full Paid"),
+    ("N", "Not Paid"),
+    ("S", "Semi Paid"),
+)
+
+class Payment(models.Model):
+    uuid = models.UUIDField(
+         primary_key = False,
+         default = uuid.uuid4,
+         editable = False)
+    purchased_by = models.OneToOneField(settings.AUTH_USER_MODEL,related_name="payment", on_delete=models.CASCADE) 
+    ticket = models.OneToOneField(Ticket,related_name="payment_info", on_delete=models.CASCADE)
+    amount_of_payment = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=1, choices=PAYE_STAUTS_CHOICES)
+    screenshot = models.ImageField(upload_to ='payment/screenshots/')
--- users/models.py
+++ users/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    phoneNumber = models.CharField(max_length=13 ,null=True)
