7a98e1fc08ab91c840b0840a531167128e07e63e
5no0p <mmoaaa1989@hotmail.com>
2021-09-25 15:39:25 +0200

    remove s from lookup_fields

--- ticket/api/v1/views.py
+++ ticket/api/v1/views.py
@@ -7,7 +7,7 @@ class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    lookup_fields = ['uuid']
+    lookup_field = ['uuid']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
