80b34f75831c8abdc081857b8347a774981a08ca
5no0p <mmoaaa1989@hotmail.com>
2021-09-26 10:30:39 +0200

    add qrcode to tickets details

--- qrcode/models.py
+++ qrcode/models.py
@@ -5,12 +5,12 @@ from django.db.models.deletion import CASCADE
 from ticket.models import Ticket
 
 
-class QRcodeGenerator(models.Model):
+class QrcodeGenerator(models.Model):
     uuid = models.UUIDField(
          primary_key = False,
          default = uuid.uuid4,
          editable = False)
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket,related_name='ticket_qrcode', on_delete=models.CASCADE)
     qrcode = models.CharField(max_length=500)
     qrimage = models.ImageField(upload_to ='QRcode/')
 
