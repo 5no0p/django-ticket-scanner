32a83639d6b5325cad779d52bccb82791bc4ff90
5no0p <mmoaaa1989@hotmail.com>
2021-10-05 18:41:25 +0200

    add *args, **kwargs to event eid save function

--- event/models.py
+++ event/models.py
@@ -27,7 +27,7 @@ class Event(models.Model):
     def __str__(self):
         return self.name
 
-    def save(self):
+    def save(self,*args, **kwargs):
         import secrets
         import string
 
